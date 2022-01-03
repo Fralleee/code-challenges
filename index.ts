32da4f5a28c0b9bef8f7c12cc87c36f8456e6be0
Roland Chelwing <jolleee@hotmail.com>
2022-01-03 11:54:12 +0100

    Remove unecessary files

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import express from 'express'
 let app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('public'))
+// app.use(express.static('public'))
 
 app.get('/', (request, response) => {
   // response.render('index');
